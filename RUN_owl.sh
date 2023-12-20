a8b94a8945a8d9dfa3f8da6aa01707f419753b9b
kaven96 <kireev.artur@gmail.com>
2023-12-20 17:51:13 +0400

    added scripts for all 3 models

---
+++ RUN_owl.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Check if all required arguments are provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <video_path> <polygon_path> <output_path> <gt_path>"
+    exit 1
+fi
+
+# Check if Python 3 is installed
+if ! command -v python3 &> /dev/null; then
+    echo "Python 3 is not installed. Please install it before proceeding."
+    exit 1
+fi
+
+# Check if deps_installed file exists
+deps_file="deps_installed"
+
+if [ ! -f "$deps_file" ]; then
+    echo "Dependencies not installed. Creating virtual environment and installing required packages..."
+
+    # Create virtual environment
+    python3 -m venv venv || { echo "Failed to create a virtual environment."; exit 1; }
+
+    # Activate virtual environment
+    source venv/bin/activate || { echo "Failed to activate the virtual environment."; exit 1; }
+
+    # Install required Python libraries
+    pip install -r requirements.txt || { echo "Failed to install Python libraries."; exit 1; }
+
+    # Mark dependencies as installed
+    touch "$deps_file"
+
+    # Deactivate virtual environment
+    deactivate
+fi
+
+# Assign input arguments to variables
+video_path=$1
+polygon_path=$2
+output_path=$3
+gt_intervals=$4
+
+# Get into virtual environment
+source venv/bin/activate
+
+echo "Processing videos..."
+python3 run_haar.py "$video_path" "$polygon_path"
+
+echo "Evaluating results"
+python3 evaluate.py "$gt_intervals" final_output_result.json
+
+# move results file to the output_path
+mv final_output_result.json "$output_path"
+
+rm output_result.json
+
+# print resulting metrics
+echo "$(cat metrics_all.txt)"
+
+echo "Detection and evaluation completed. Results saved to: $output_path"
---
+++ run_owl.py
@@ -0,0 +1,57 @@
+import os
+import argparse
+from detect2 import VehicleDetector
+import json
+
+
+def process_videos(video_list, polygons_path):
+    results_dict = {}
+
+    for video_name in video_list:
+        print(f"Processing video: {video_name}")
+        detector = VehicleDetector(
+            video_name,
+            polygons_path
+        )
+        detector.load_video()
+        detector.load_polygons()
+        detector.detect_vehicles()
+
+        with open("output_result.json", "r") as json_file:
+            current_result = json.load(json_file)
+
+        results_dict.update(current_result)
+
+    with open("final_output_result.json", "w") as json_file:
+        json.dump(results_dict, json_file)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Process videos and polygons file.")
+    parser.add_argument(
+        "video_list",
+        help="Path to the video file or folder containing video files.",
+    )
+    parser.add_argument(
+        "polygons_path",
+        help="Path to the polygons file.",
+    )
+    args = parser.parse_args()
+
+    if os.path.isdir(args.video_list):
+        video_list = [
+            os.path.join(args.video_list, video)
+            for video in os.listdir(args.video_list)
+            if video.endswith(".mp4")
+        ]
+    elif os.path.isfile(args.video_list) and args.video_list.endswith(".mp4"):
+        video_list = [args.video_list]
+    else:
+        print("Invalid input. Please provide a valid video file or folder for input 1.")
+        return
+
+    process_videos(video_list, args.polygons_path)
+
+
+if __name__ == "__main__":
+    main()
